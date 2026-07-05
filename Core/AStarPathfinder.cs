2368f0cb56bb9c316012566552c5e7407b128b9b
Aleksandra Kenig <aleks.creatrix@gmail.com>
2026-07-05 07:50:34 +0200

    Create algorithm A*

---
+++ Core/AStarPathfinder.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace AStarShowcase.Core
+{
+    public class PathResult<TNode> where TNode : INode
+    {
+        public bool Success { get;}
+        public List<TNode> Path { get; }
+        public IReadOnlyCollection<TNode> ExploredNodes { get;}
+        public int NodesEvaluated { get;}
+        public double ElapsedMilliseconds { get;}
+        public PathResult(bool success, List<TNode> path, IReadOnlyCollection<TNode> exploredNodes, int nodesEvaluated, double elapsedMilliseconds)
+        {
+            Success = success;
+            Path = path;
+            ExploredNodes = exploredNodes;
+            NodesEvaluated = nodesEvaluated;
+            ElapsedMilliseconds = elapsedMilliseconds;
+        }
+    }
+    public class AStarPathfinder<TNode> where TNode : IHasXmlNode
+    {
+        private readonly IGrid<TNode> _grid;
+        public AStarPathfinder(IGrid<TNode> grid)
+        {
+            _grid = grid ?? throw new ArgumentNullException(nameof(grid));
+        }
+        public PathResult<TNode> FindPath(TNode start, TNode goal)
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            if (!start.IsWalkable || !goal.IsWalkable)
+            {
+                return new PathResult<TNode>(false, new List<TNode>(), new List<TNode>(), 0, stopwatch.Elapsed.TotalMilliseconds);
+            }
+            var gScore = new Dictionary<TNode, float>{[start] = 0f};
+            var cameFrom = new Dictionary<TNode, TNode>();
+            var fScore = new Dictionary<TNode, float>{[start] = _grid.GetHeuristic(start, goal)};
+            var openSet = new BinaryHeap<TNode>((a, b) => fScore[a].CompareTo(fScore[b]));
+            openSet.Insert(start);
+            var closedSet = new HashSet<TNode>();
+            int nodesEvaluated = 0;
+            while (openSet.count > 0)
+            {
+                TNode current = openSet.ExtraMin();
+                nodesEvaluated++;
+                if (Equals(current, goal))
+                {
+                    stopwatch.Stop();
+                    var path = ReconstructionPAth(cameFrom, current);
+                    return new PathResult<TNode>(true, path, closedSet, nodesEvaluated, stopwatch.Elapsed.TotalMilliseconds);
+                }
+                closedSet.Add(current);
+                foreach (TNode neighbor in _grid.GetNeighbors(current))
+                {
+                    if (closedSet.Contains(neighbor)) continue;
+                    float tenmtativeGScore = gScore[current] + _grid.GetMovementCost(current, neighbor);
+                    bool neighborIsNew = !gScore.ContainsKey(neighbor);
+                    if (neighborIsNew || tenmtativeGScore < gScore[neighbor])
+                    {
+                        cameFrom[neighbor] = current;
+                        gScore[neighbor] = tentativeGScore;
+                        fScore[neighbor] = tentativeGScore + _grid.GetHeuristic(neighbor, goal);
+                        if (neighborIsNew) openSet.Insert(neighbor);
+                        else openSet.UpdatePriority(neighbor);
+                    }
+                }
+            }
+            stopwatch.Stop();
+            return new PathResult<TNode>(false, new List<Tnode>(), closedSet, nodesEvaluated, stopwatch.Elapsed.TotalMilliseconds);
+        }
+        private static List<TNode> ReconstructPath(Dictionary<TNode, TNode> cameFrom, TNode current)
+        {
+            var path = new List<TNode> { current};
+            while (cameFrom.TryGetValue(current, out TNode previous))
+            {
+                current = previous;
+                path.Add(current);
+            }
+            path.Reverse;
+            return path;
+        }
+    }
+}
